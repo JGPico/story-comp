eb4bc3ae47f526672b53ec4cf830927cec45da45
JGPico <minq_2@hotmail.com>
2026-06-24 12:58:12 -0600

    server plan

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,36 @@ import express from 'express'
 const app = express()
 const port = Number(process.env.PORT) || 5432
 
+// TODO Create a Dockerfile
+// TODO Create a docker-compose.yml file
+// TODO Create a .dockerignore file
+// TODO Create a .dockerfile file
+
+// TODO: Add routes
+// TODO: Add error handling
+// TODO: Add health check
+// TODO: Add endpoints
+
+// TODO: Add database
+// TODO: Add authentication
+// TODO: Add authorization
+// TODO: Add logging
+// TODO: Add monitoring
+// TODO: Add security
+// TODO: Add monitoring
+
+// TODO: Add middleware to parse JSON bodies
+// TODO: Add middleware to log requests
+// TODO: Add middleware to add CORS headers
+// TODO: Add middleware to add security headers
+// TODO: Add middleware to add rate limiting
+// TODO: Add middleware to add error handling
+// TODO: Add middleware to add logging
+// TODO: Add middleware to add monitoring
+// TODO: Add middleware to add security
+
+// TODO: Add deployment
+
 app.get('/', (_req, res) => {
   res.send('You have reached the api for story comp')
 })
